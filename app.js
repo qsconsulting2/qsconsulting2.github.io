3283728ffbbf075be187e5ab93a99f3084698c46
Tim C <timoman8@hotmail.com>
2018-05-01 12:17:26 -0400

    Added edit post

--- app.js
+++ app.js
@@ -204,6 +204,15 @@
     }
   });
   window.addEventListener('load', function(){
+    let _queryObject = window.location.search.split(/\?|\&/g).reverse();
+    let queryObject = {};
+    _queryObject.pop();
+    _queryObject = _queryObject.reverse();
+    _queryObject.forEach((k)=>{
+        let kvp = k.split('=');
+        queryObject[kvp[0]] = kvp[1];
+    });
+    window.location.query = queryObject;
     let checklist = {
       'listeningToLogin': false,
       'listeningToLogout': false,
--- post/js/post.js
+++ post/js/post.js
@@ -1,5 +1,5 @@
 (function Post(){
-    function _createPost(title, content, tags){
+    function createPost(title, content, tags){
         let fbRef = firebase.database().ref('posts');
         let postID = fbRef.push().key;
         let postData = {
@@ -7,6 +7,18 @@
             "content": content,
             "tags": tags,
             "time": Date.now(),
+            "views": 0
+        };
+        fbRef.child(postID).set(postData);
+    }
+    function updatePost(title, content, tags, post_time, post_views, _postID){
+        let fbRef = firebase.database().ref('posts');
+        let postID = _postID;
+        let postData = {
+            "title": title,
+            "content": content,
+            "tags": tags,
+            "time": post_time,
             "views": 0
         };
         fbRef.child(postID).set(postData);
@@ -19,10 +31,36 @@
         let submit_post = document.querySelector('#submitPost');
         let post_title = document.querySelector('#postTitle');
         let post_tags = document.querySelector('#postTags');
-        post_tags.setAttribute('title', 'Tags are separated by a comma')
+        let post_time;
+        let post_views;
+        let _postID_;
+        post_tags.setAttribute('title', 'Tags are separated by a comma');
+        if(window.location.query.postid != undefined){
+            firebase.database().ref(`posts/${window.location.query.postid}`).once('value', (v)=>{
+                let _post = v.val();
+                if(_post != null){
+                    submit_post.innerHTML = "Update";
+                    myEditor.instanceById('textarea-create-post').setContent(_post.content);
+                    post_title.value = _post.title;
+                    post_tags.value = _post.tags.join(', ');
+                    post_time = _post.time;
+                    post_views = _post.views;
+                    _postID_ = window.location.query.postid;
+                }
+            });
+        }
         submit_post.addEventListener('click', function(){
-            let post_content = myEditor.instanceById('textarea-create-post').getContent();
-            _createPost(post_title.value, post_content, post_tags.value.replace(/, /g,',').split(','));
+            if(window.location.query.postid != undefined){
+                let post_content = myEditor.instanceById('textarea-create-post').getContent();
+                updatePost(post_title.value, post_content, post_tags.value.replace(/, /g,',').split(','), post_time, post_views, window.location.query.postid);
+                myEditor.instanceById('textarea-create-post').setContent('<br/>');
+                submit_post.innerHTML = "Submit";
+            }else{
+                let post_content = myEditor.instanceById('textarea-create-post').getContent();
+                createPost(post_title.value, post_content, post_tags.value.replace(/, /g,',').split(','));
+                myEditor.instanceById('textarea-create-post').setContent('<br/>');
+                submit_post.innerHTML = "Submit";
+            }
         }, false);
     }, false);
 })();
